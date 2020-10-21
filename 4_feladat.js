c02b645577a35d2a2673e4ad484cf1bfdfdc98cd
Dominik <gosidominik@gmail.com>
2020-10-21 10:07:01 +0200

    4_feladat added

---
+++ 4_feladat.js
@@ -0,0 +1,16 @@
+const minMax = (arr) => {
+	let min = arr[0];
+	let max = arr[0]
+	for (let i = 1; i < arr.length; i++) {
+		if (min > arr[i]) {
+			min = arr[i];
+		}
+		if (max < arr[i]) {
+			max = arr[i];
+		}
+	}
+	return {
+		minimum: min,
+		maximum: max
+		}
+};
